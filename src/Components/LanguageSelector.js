7f75461d7aca9b8eeea160c3497c2136135ba758
Rashmit Pankhania <raspankh@publicisgroupe.net>
2021-01-26 20:30:52 +0530

    language translation added

---
+++ src/Components/LanguageSelector.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const LanguageSelector = (props) => {
+    const {langValue, setLangValue} = props;
+    return (
+        <>
+            <label htmlFor="Select Language" style={{padding: '5px', fontSize: '10px'}}>Select Language</label>
+            <select className="form-select" aria-label="Default select example" name="Select Language"
+                    onChange={e => setLangValue(e.target.value)}>
+                <option value={-1} selected={langValue === -1}>English</option>
+                <option value={0} selected={langValue === 0}>Aragonese</option>
+                <option value={1} selected={langValue === 1}>Catalan</option>
+                <option value={2} selected={langValue === 2}>Corsican</option>
+                <option value={3} selected={langValue === 3}>English</option>
+                <option value={4} selected={langValue === 4}>French</option>
+                <option value={5} selected={langValue === 5}>Galician</option>
+                <option value={6} selected={langValue === 6}>Italian</option>
+                <option value={7} selected={langValue === 7}> Latin</option>
+                <option value={8} selected={langValue === 8}>Occitan</option>
+                <option value={9} selected={langValue === 9}>Portuguese</option>
+                <option value={10} selected={langValue === 10}>Romansh</option>
+                <option value={11} selected={langValue === 11}>Romanian</option>
+                <option value={12} selected={langValue === 12}> Spanish</option>
+                <option value={13} selected={langValue === 13}>Wallon</option>
+            </select>
+        </>
+    )
+}
+
+export default LanguageSelector;
